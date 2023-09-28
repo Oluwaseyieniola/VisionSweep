b9d17b1086a47ae80c0b7e88b6950d8902ed115b
Oluwaseyi Ogunjinmi <80404459+Oluwaseyieniola@users.noreply.github.com>
2023-09-28 14:27:17 +0100

    build

---
+++ flutter_pytorch/generated/source/buildConfig/debug/com/aneeqmalik/flutter_pytorch/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aneeqmalik.flutter_pytorch;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.aneeqmalik.flutter_pytorch";
+  public static final String BUILD_TYPE = "debug";
+}
